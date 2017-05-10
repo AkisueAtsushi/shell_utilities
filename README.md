cb13250281a840e6407164b644a2b26d9f4875c3
Atsushi Akisue <AkisueAtsushi@users.noreply.github.com>
2017-05-10 22:56:48 +0900

    1st commit for defunctionalized

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ Any useful shell scripts for maintaining server
 
 ### ping_check.sh
 
-it only contains "ping_check" function.  
+To check ping status. 
 
 Usage:  
-	$1=destination IP   
+	$1=destination IP  
 	$2=the limit of retry(optional, default: 5)  
 	$3=retry interval(optional, [smhd]option available, default: 10s)  
 
@@ -19,15 +19,13 @@ Example:
 
 DESTINATION_IP=192.168.0.1
 
-. /Directory/to/your/ping_check.sh
-
 #retry limit: 5times(default), retry interval: 10seconds(default)
-ping_check $DESTINATION_IP
+/Directory/to/your/ping_check.sh $DESTINATION_IP
 
 #retry limit: 10times, retry interval: 5seconds
-ping_check $DESTINATION_IP 10 5s
+/Directory/to/your/ping_check.sh $DESTINATION_IP 10 5s
 ```
 
-Result:  
-Success: Return empty.  
+Result:
+Success: Return empty.
 Failure: Output ERROR message to stderr.
--- ping_check.sh
+++ ping_check.sh
@@ -7,56 +7,54 @@ set -ue -o pipefail
 export LC_ALL=C
 export LANG=C
 
-ping_check() {
 
-  #validation - number of arguments 
-  if [ $# -eq 0 ]; then
-    cat <<EOF
-      ping_check() is a tool for ...
+#validation - number of arguments 
+if [ $# -eq 0 ]; then
+  cat <<EOF
+    ping_check() is a tool for ...
 
-      Usage:
-        ping_check [destination IP] [the limit of retry(optional, default:5)] [retry interval((optional, [smhd]option available, default: 10s)]
+    Usage:
+      ping_check [destination IP] [the limit of retry(optional, default:5)] [retry interval((optional, [smhd]option available, default: 10s)]
 EOF
 
-    exit 0 
-  fi
+  exit 0 
+fi
 
-  #$1 IP validation
-  local DESTINATION_IP=$1
-  IP_CHECK=$(echo ${DESTINATION_IP} | egrep "^(([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
+#$1 IP validation
+readonly DESTINATION_IP=$1
+IP_CHECK=$(echo ${DESTINATION_IP} | egrep "^(([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
 
-  if [ ! "${IP_CHECK}" ] ; then
-    echo "[ERROR] $DESTINATION_IP is not IP Address." 1>&2
-    exit 1
-  fi
+if [ ! "${IP_CHECK}" ] ; then
+  echo "[ERROR] $DESTINATION_IP is not IP Address." 1>&2
+  exit 1
+fi
 
-  #$2 Natural number validation
-  local readonly RETRY_LIMIT=${2:-5}
-  expr ${RETRY_LIMIT} + 1 > /dev/null 2>&1
-  if [ $? -ge 2 -o $RETRY_LIMIT -lt 0 ]; then
-    echo "$RETRY_LIMIT is not natural number." 1>&2
-    exit 1
-  fi
+#$2 Natural number validation
+readonly RETRY_LIMIT=${2:-5}
+expr ${RETRY_LIMIT} + 1 > /dev/null 2>&1
+if [ $? -ge 2 -o $RETRY_LIMIT -lt 0 ]; then
+  echo "$RETRY_LIMIT is not natural number." 1>&2
+  exit 1
+fi
 
-  #$3 sleep seconds
-  local readonly SLEEP_TIME=${3:-"10s"}
+#$3 sleep seconds
+readonly SLEEP_TIME=${3:-"10s"}
  
-  local retry_count=0
-  while :
-  do
-    /bin/ping $DESTINATION_IP -c 1 > /dev/null 2>&1
+retry_count=0
+while :
+do
+  /bin/ping $DESTINATION_IP -c 1 > /dev/null 2>&1
 
-    if [ $? -eq 0 ]; then
-      break
-    else
-      let retry_count++
+  if [ $? -eq 0 ]; then
+    break
+  else
+    let retry_count++
 
-      if [ $retry_count -eq $RETRY_LIMIT ]; then
-        echo "ping did not reached to $DESTINATION_IP" 1>&2
-        exit 1
-      fi
+    if [ $retry_count -eq $RETRY_LIMIT ]; then
+      echo "ping did not reached to $DESTINATION_IP" | /root/util/webhooks.sh -m "ping failure" -i ":ng:"
+      exit 1
     fi
+  fi
 
-    /bin/sleep $SLEEP_TIME
-  done
-}
+  /bin/sleep $SLEEP_TIME
+done
